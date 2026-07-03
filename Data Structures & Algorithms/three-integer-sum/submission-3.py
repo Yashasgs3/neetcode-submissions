c590e4ab27688105a3d2288ce840d97cb55d8fa3
Yashas G S <139982934+Yashasgs3@users.noreply.github.com>
2026-07-03 20:20:29 +0530

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.py
@@ -0,0 +1,21 @@
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        result = set()
+
+        for i in range(len(nums)):
+
+            seen = set()
+
+            for j in range(i + 1, len(nums)):
+
+                third = -(nums[i] + nums[j])
+
+                if third in seen:
+                    triplet = tuple(sorted([nums[i], nums[j], third]))
+                    result.add(triplet)
+
+                seen.add(nums[j])
+
+        return [list(triplet) for triplet in result]
